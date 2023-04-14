7b0035cca933e48f48a5e7f4a35fee911106a912
PoreetSSP <130555057+PoreetSSP@users.noreply.github.com>
2023-04-15 00:47:47 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@
 #include <ctime>
 
 int width, height, numberOfParticles;
+int i = 0;
+int j = 0;
 int steps = 0;
-int MaxSteps = 100000;
 int ParticleInit = 0;
 
 std::random_device rd;
@@ -16,38 +17,38 @@ std::mt19937 gen(rd());
 std::uniform_int_distribution<> dis(1, 8);
 
 //is particle on the edge of the grid
-bool isOnEdge(int i,int j,const std::vector<std::vector<bool>>& grid) {
-  if (i <= 0 || i >= height - 1 || j <= 0 || j >= width - 1) return true;
+bool isOnEdge(int i,int j) {
+  if (i == 0 || i == height - 1 || j == 0 || j == width - 1) return true;
   else {
     return false;
   }
 }
- 
+
+bool inBounds(int i, int j){
+  if (i > 0 && i < height - 1 && j > 0 && j < width - 1) return true;
+  return false;
+}
+
 //is particle next to center particle?
 bool hasDeadNeighbour(int i, int j, const std::vector<std::vector<bool>>& grid) {
   // Check if the particle is adjacent to the center particle
-  int middleI = height / 2;
-  int middleJ = width / 2;
-
-    std::vector<std::pair<int, int>> coordsToCheck { //make a pair of 2 coordinates
+    std::vector<std::pair<int, int>> coordsToCheck { //make a pair of 2 displacements di en dj
         {1, 0}, {0, -1}, {0, 1}, {-1, 0},
         {1, 1}, {1, -1}, {-1, 1}, {-1, -1} 
     };
 
-    for (std::pair<int, int> coords : coordsToCheck) { 
-        if (grid[middleI + coords.first][middleJ + coords.second]) { //bv grid[height/2 +1][width/2 +0]
+    for (std::pair<int, int> coords : coordsToCheck) { //loopt over elk element van de array  //loop variabele : vector
+        if (inBounds(i+coords.first,j + coords.second) && grid[i + coords.first][j + coords.second]) { //bv grid[height/2 +1][width/2 +0]
             return true;
         }
     }
     return false;
 }
 
-}
 
 void MoveParticle(int& i, int& j, std::vector<std::vector<bool>>& grid){
-
-  }
-  if (isOnEdge(i, j, grid)){
+ 
+  if (isOnEdge(i, j)){
     if (j == 0) {
       j++;
     } else if (j == width - 1) {
@@ -56,12 +57,12 @@ void MoveParticle(int& i, int& j, std::vector<std::vector<bool>>& grid){
       i++;
     } else {
       i--;
-    }  grid[i][j] = true;
+    }  
   } else{
       int prev_i = i;
       int prev_j = j;
       grid[prev_i][prev_j] = false; // remove previous position from grid
-    
+        
       int direction = dis(gen);
 
       if (direction == 1) {
@@ -85,11 +86,10 @@ void MoveParticle(int& i, int& j, std::vector<std::vector<bool>>& grid){
         i++;
         j--;
       }
-     
-      if (hasDeadNeighbour(i, j, grid)) {
+  }
+   if (hasDeadNeighbour(i, j, grid)) {
         grid[i][j] = true;
       }
-  }
 }
   
 
@@ -110,42 +110,38 @@ int main(){
   }
   
   // making the center particle true:
-  const int x = width / 2;
-  const int y = height / 2;
-  grid[x][y] = true;
+  const int m = height/ 2;
+  const int n = width / 2;
+  grid[m][n] = true;
 
    // Seed the random number generator with the current time
     std::srand(std::time(nullptr));
 
-
   while (ParticleInit < numberOfParticles) {
-    steps = 0;
-    for (int k = 0; k < height; k++){
-      int i = 0;
-      int j = 0;
-      
+   steps = 0;
+    for (int k = 0; k < numberOfParticles; k++){
+      i = 0;
+      j = 0;
+       
       //starting position of the particle:
-      int side = std::rand() % 4 + 1;  // Generate a random number between 1 and 4
+      int side = (std::rand() % 4) + 1;  // Generate a random number between 1 and 4
       if (side == 1){ // i , j=0
         // Choose a random row index on the chosen column
-          i = std::rand() % (height - 1);
-            grid[i][0] = true;
+          i = std::rand() % (height);
       } else if (side ==2){ //i , j = width -1
-          i = std::rand() % (height - 1);
-            grid[i][width-1] = true;
+          i = std::rand() % (height);
       } else if(side == 3){ //i=0, j
-        j = std::rand() % (width - 1);
-          grid[0][j] = true;
+        j = std::rand() % (width);
       } else { //i = height -1, j
-        j = std::rand() % (width - 1);
-          grid[height-1][j] = true;
+        j = std::rand() % (width);
       }
       
-      while (!IsAdjacentToCenter(i, j, grid) && !isAdjacentToCluster(i,j,grid) && steps < MaxSteps) {
+      while (!hasDeadNeighbour(i,j,grid)) {
         MoveParticle(i, j, grid);
         steps++;
+        //print to console
       }
-        ParticleInit++;
+      ParticleInit++;
     }
   }
 
